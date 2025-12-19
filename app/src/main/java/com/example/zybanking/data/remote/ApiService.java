a0a0699b1ccea0f380621834e01d9b27acc1efd7
huynhthu214 <minhthuhuynh23@gmail.com>
2025-12-19 17:39:38 +0700

    merge

--- app/src/main/java/com/example/zybanking/data/remote/ApiService.java
+++ app/src/main/java/com/example/zybanking/data/remote/ApiService.java
@@ -1,14 +1,24 @@
 package com.example.zybanking.data.remote;
 
 import com.example.zybanking.data.models.AccountSummaryResponse;
+import com.example.zybanking.data.models.BasicResponse;
+import com.example.zybanking.data.models.BillListResponse;
+import com.example.zybanking.data.models.BillPayRequest;
+import com.example.zybanking.data.models.BillResponse;
+import com.example.zybanking.data.models.DepositRequest;
 import com.example.zybanking.data.models.ForgotPasswordRequest;
 import com.example.zybanking.data.models.ForgotPasswordResponse;
 import com.example.zybanking.data.models.LoginRequest;
 import com.example.zybanking.data.models.LoginResponse;
+import com.example.zybanking.data.models.OtpConfirmRequest;
 import com.example.zybanking.data.models.ResetPasswordRequest;
 import com.example.zybanking.data.models.Transaction;
+import com.example.zybanking.data.models.TransferRequest;
 import com.example.zybanking.data.models.UserResponse;
+import com.example.zybanking.data.models.UtilityResponse;
+import com.example.zybanking.data.models.UtilityTopupRequest;
 import com.example.zybanking.data.models.VerifyOtpRequest;
+import com.example.zybanking.data.models.WithdrawRequest;
 
 import java.util.List;
 import java.util.Map;
@@ -55,6 +65,42 @@ public interface ApiService {
             @Query("user_id") String userId,
             @Query("limit") int limit
     );
+
+    @POST("transactions/transfer/create")
+    Call<BasicResponse> transferCreate(@Body TransferRequest body);
+
+    @POST("transactions/transfer/confirm")
+    Call<BasicResponse> transferConfirm(@Body OtpConfirmRequest body);
+
+    @POST("transactions/deposit/create")
+    Call<BasicResponse> deposit(@Body DepositRequest body);
+
+    @POST("transactions/deposit/confirm")
+    Call<BasicResponse> depositConfirm(@Body OtpConfirmRequest body);
+
+    @POST("transactions/withdraw/create")
+    Call<BasicResponse> withdraw(@Body WithdrawRequest body);
+
+    @POST("transactions/withdraw/confirm")
+    Call<BasicResponse> withdrawConfirm(@Body OtpConfirmRequest body);
+
+    // BILL
+    @GET("bills")
+    Call<BillListResponse> listBills(@Query("keyword") String keyword);
+
+    @GET("bills/{bill_id}")
+    Call<BillResponse> getBill(@Path("bill_id") String billId);
+
+    @POST("bills/pay")
+    Call<BasicResponse> payBill(@Body BillPayRequest body);
+
+    // UTILITY
+    @POST("utility/topup")
+    Call<BasicResponse> utilityTopup(@Body UtilityTopupRequest body);
+    @POST("utility/confirm")
+    Call<BasicResponse> utilityConfirm(@Body OtpConfirmRequest body);
+    @GET("utility/{utility_payment_id}")
+    Call<UtilityResponse> getUtilityDetail(@Path("utility_payment_id") String id);
 
 }
 
--- app/src/main/java/com/example/zybanking/ui/transaction/TransactionActivity.java
+++ app/src/main/java/com/example/zybanking/ui/transaction/TransactionActivity.java
@@ -1,14 +1,24 @@
 package com.example.zybanking.ui.transaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.zybanking.NavbarActivity;
 import com.example.zybanking.R;
+import com.example.zybanking.data.models.BasicResponse;
+import com.example.zybanking.data.models.WithdrawRequest;
+import com.example.zybanking.data.remote.ApiService;
+import com.example.zybanking.ui.auth.OtpVerificationActivity;
 
-public class TransactionActivity extends NavbarActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TransactionActivity extends AppCompatActivity {
+    private ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,32 @@ public class TransactionActivity extends NavbarActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.basic_transfer);
 
-        initNavbar();
+        WithdrawRequest request = new WithdrawRequest("A001", 100000);
+
+        apiService.withdraw(request).enqueue(new Callback<BasicResponse>() {
+
+            @Override
+            public void onResponse(Call<BasicResponse> call,
+                                   Response<BasicResponse> response) {
+
+                if (response.isSuccessful()) {
+                    String txId = response.body().transaction_id;
+
+                    Intent i = new Intent(
+                            TransactionActivity.this,
+                            OtpVerificationActivity.class
+                    );
+                    i.putExtra("transaction_id", txId);
+                    startActivity(i);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BasicResponse> call, Throwable t) {
+                Toast.makeText(TransactionActivity.this,
+                        t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
